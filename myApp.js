ce6fdc63e9c5d385d1dad066ecd0bb81cb75a598
Areeb Qazi <saka-rauka1@hotmail.co.uk>
2022-07-05 00:29:35 +0100

    json served is now modified by env variables

--- myApp.js
+++ myApp.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 let express = require('express');
 let app = express();
 console.log("Hello World");
@@ -6,13 +8,15 @@ app.get("/", (GET, res) => {
     res.sendFile(__dirname + "/views/index.html")
 });
 
-app.use("/public", express.static(__dirname + "/public"))
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (GET, res) => {
-    res.json({"message": "Hello json"})
+    let jsonResponse = "Hello json";
+    if (process.env.MESSAGE_STYLE == "uppercase") {
+        jsonResponse = jsonResponse.toUpperCase();
+    }
+    res.json({ "message": jsonResponse});
 });
-
-
 
 
 
