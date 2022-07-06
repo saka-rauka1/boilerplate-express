41068eca4ee6522c28f7e13b27ae9558ff1f1147
Areeb Qazi <saka-rauka1@hotmail.co.uk>
2022-07-06 02:22:53 +0100

    Chained middleware to create a time server

--- myApp.js
+++ myApp.js
@@ -23,7 +23,12 @@ app.get("/json", (GET, res) => {
     res.json({ "message": jsonResponse});
 });
 
-
+app.get("/now", (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({ "time": req.time });
+});
 
 
 
