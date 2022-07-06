6d59eb675b7287ba8d665af04496a8c764bf17ef
Areeb Qazi <saka-rauka1@hotmail.co.uk>
2022-07-06 02:10:53 +0100

    Added root-level request logger middleware

--- myApp.js
+++ myApp.js
@@ -4,6 +4,11 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
+app.use((req, res, next) => {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+});
+
 app.get("/", (GET, res) => {
     res.sendFile(__dirname + "/views/index.html")
 });
